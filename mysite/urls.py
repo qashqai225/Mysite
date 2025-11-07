0d719dba3132c45f6266905217923548a1a52cb3
Genest <242404530+qashqai225@users.noreply.github.com>
2025-11-07 23:50:28 +0100

    check 3.1

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from users import views as userViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
+    path('register/', userViews.register, name='register'),
+    path('', include('blog.urls'))
 ]
