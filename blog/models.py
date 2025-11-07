0b3b4710c28f09a3095e556a88a8dbc4dc54b3f2
Genest <242404530+qashqai225@users.noreply.github.com>
2025-11-07 21:38:30 +0100

    check2

--- blog/models.py
+++ blog/models.py
@@ -8,5 +8,4 @@ class News(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.title
+    
